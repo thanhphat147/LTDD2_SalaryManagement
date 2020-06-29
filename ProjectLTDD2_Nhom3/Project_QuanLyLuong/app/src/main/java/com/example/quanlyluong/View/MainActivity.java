03d2880b024fbe77944c341ff7fba381aef385aa
Thành Phát <thanhphat147@gmail.com>
2020-06-29 18:27:10 +0700

    change file

--- ProjectLTDD2_Nhom3/Project_QuanLyLuong/app/src/main/java/com/example/quanlyluong/View/MainActivity.java
+++ ProjectLTDD2_Nhom3/Project_QuanLyLuong/app/src/main/java/com/example/quanlyluong/View/MainActivity.java
@@ -1,9 +1,10 @@
-package com.example.quanlyluong;
+package com.example.quanlyluong.View;
 
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
 
+import com.example.quanlyluong.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
